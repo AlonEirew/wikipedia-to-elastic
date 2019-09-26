a1e78929079ae22591af23081742e6002c636a2d
AlonEirew <alon.eirew@intel.com>
2019-09-26 10:09:55 +0300

    fix docker build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 7z x /tmp/elastic-dump.7z -o/tmp
 
-curl -H "Content-Type: application/json" -d @tmp/elastic-dump/enwiki_settings.json -X PUT http://localhost:9200/enwiki_v3
+elasticdump --input=/tmp/elastic-dump/enwiki_analyzer.json --output=http://localhost:9200/enwiki_v3 --type=analyzer
 elasticdump --input=/tmp/elastic-dump/enwiki_mapping.json --output=http://localhost:9200/enwiki_v3 --type=mapping
 elasticdump --input=/tmp/elastic-dump/enwiki_data.json --output=http://localhost:9200/enwiki_v3 --type=data
 
